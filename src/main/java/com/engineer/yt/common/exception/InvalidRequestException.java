6259c6c4020e523c54abfaedfa3c767652669bb5
Binh Nguyen Van <nvbinhsoft@gmail.com>
2023-05-10 22:49:23 +0700

    feat: create exception type

---
+++ src/main/java/com/engineer/yt/common/exception/InvalidRequestException.java
@@ -0,0 +1,12 @@
+package com.engineer.yt.common.exception;
+
+
+
+public class InvalidRequestException extends RuntimeException {
+
+    private static final long serialVersionUID = 7736869550788854505L;
+
+    public InvalidRequestException(int code, String message) {
+        super();
+    }
+}
